1afcf99a3c3df45f7aa90a46f027eba249f06347
huxinliang <huxinliang2019@ia.ac.cn>
2020-09-10 16:26:14 +0800

    mask rcnn

---
+++ mask_rcnn/model.py
@@ -0,0 +1,89 @@
+import torchvision
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
+import torch.nn as nn
+
+def get_model_instance_segmentation(num_classes):
+    # 加载在coco上预训练好的实例分割模型
+    model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True)
+
+    # 获得分类器的输入特征数
+    in_feature = model.roi_heads.box_predictor.cls_score.in_features
+    # 用新的头部替换预先训练好的头部
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_feature, num_classes=num_classes)
+
+    # 现在获取掩码分类器的输入特征数
+    in_feature_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
+    hidden_layer = 256
+    # 用新的掩码预测器替换掩码预测期
+    model.roi_heads.mask_predictor = MaskRCNNPredictor(in_feature_mask, hidden_layer, num_classes)
+    return model
+
+import mask_rcnn.detection.transforms as T
+
+def get_transform(train):
+    transforms = []
+    transforms.append(T.ToTensor())
+    if train:
+        transforms.append(T.RandomHorizontalFlip(0.5))
+    return T.Compose(transforms)
+
+from mask_rcnn.detection.engine import train_one_epoch, evaluate
+import mask_rcnn.detection.utils as utils
+import mask_rcnn.dataset as mydataset
+import torch.utils.data
+import os
+import torch
+
+def main():
+    # cuda
+    os.environ["CUDA_VISIBLE_DEVICES"] = '4,5,6,7'
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    num_classes=2
+    #  使用我们的数据集和定义的转换
+    dataset = mydataset.PennFudanDataset('../PennFudanPed', get_transform(train=True))
+    dataset_test = mydataset.PennFudanDataset('../PennFudanPed', get_transform(train=False))
+
+    # 在训练和测试集中拆分数据集
+    indices = torch.randperm(len(dataset)).tolist()
+    dataset = torch.utils.data.Subset(dataset, indices[:-50])
+    dataset_test = torch.utils.data.Subset(dataset_test, indices[-50:])
+
+    # 定义训练和验证数据加载器
+    data_loader = torch.utils.data.DataLoader(
+        dataset, batch_size=2, shuffle=True, num_workers=4, collate_fn=utils.collate_fn
+    )
+
+    data_loader_test = torch.utils.data.DataLoader(
+        dataset_test, batch_size=1, shuffle=False, num_workers=4, collate_fn=utils.collate_fn
+    )
+
+    # 使用我们的辅助函数获得模型
+    model = get_model_instance_segmentation(num_classes=num_classes)
+
+    # cuda
+    model.cuda()
+    # model = nn.DataParallel(model)
+
+    # 构建一个优化器
+    params = [p for p in model.parameters() if p.requires_grad]
+    optimizer = torch.optim.SGD(params, lr=0.005, momentum=0.9, weight_decay=0.0005)
+
+    # 学习率调度程序
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
+
+    # 训练10个epoch
+    num_epochs = 10
+    for epoch in range(num_epochs):
+        # 训练一个epoch，每10个迭代打印一次
+        train_one_epoch(model, optimizer, data_loader, device=device,
+                        epoch = epoch, print_freq=10)
+        # 更新学习率
+        lr_scheduler.step()
+        # 在测试集上评价
+        evaluate(model, data_loader_test, device=device)
+
+    print("That's is!")
+
+if __name__ == '__main__':
+    main()
---
+++ mask_rcnn/model_learn.py
@@ -0,0 +1,54 @@
+###############################################################################
+###################### 场景1 微调已经预训练的模型 ################################
+###############################################################################
+# 我们想要预训练好的模型，只微调最后一层
+import torchvision
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+
+# 在COCO上加载经过预训练的模型
+model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+
+# 将分类器替换为用户定义的num_classes的新分类器
+num_classes = 2 # 1 class(person) + backgroud
+# 获得分类器的输入参数数量
+in_features = model.roi_heads.box_predictor.cls_score.in_features
+# 用新的头部替换预先训练好的头部
+model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+###############################################################################
+###################### 场景2 修改模型以添加不同的主干 ############################
+###############################################################################
+# 我们想要用不同的模型替换主干
+import torchvision
+from torchvision.models.detection import FasterRCNN
+from torchvision.models.detection.rpn import AnchorGenerator
+
+# 加载预先训练的模型进行分类和返回
+# 只有功能
+backbone = torchvision.models.mobilenet_v2(pretrained=True).features
+
+# # print(backbone)
+# backbone1 = torchvision.models.resnet50(pretrained=True)
+# # print(backbone1)
+# # print(type(backbone1))
+# model_ft = torchvision.models.resnet18(pretrained=True) #加载预训练网络
+
+# FasterRCNN需要知道骨干网中的输出通道数量。对于mobilenet_v2，他是1280，所以我们需要在这里添加它
+backbone.out_channels = 1280
+
+# 我们让RPN在每个空间位置生成5x3个锚点
+# 具有5种不同大小和3种不用的宽高比
+# 我们有一个元祖[元祖[int]]
+# 因为每个特征映射可能具有不同的大小和宽高比
+anchor_generotor = AnchorGenerator(sizes=((32, 64, 128, 256, 512),),
+                                   aspect_ratios=((0.5, 1.0, 2.0), ))
+
+# 定义一下我们将用于执行感兴趣区域裁剪的特征映射，以及重新缩放后裁剪的大小
+# 如果你的主干返回Tensor，则featmap_names应为[0]
+# 更一般地，主干应该返回OrderedDict[Tensor]
+# 并且在featmap_names中，你可以选择要使用的功能映射
+roi_pooler = torchvision.ops.MultiScaleRoIAlign(featmap_names=[0],
+                                                output_size=7,
+                                                sampling_ratio=2)
+# 将这些pieces放在fasterrcnn模型中
+model = FasterRCNN(backbone, num_classes=2, rpn_anchor_generator=anchor_generotor, box_roi_pool=roi_pooler)
